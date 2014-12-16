586db0b5ad77394d2639cb768338f7a5abb2ec05
Elvis Nuñez <elvisnunez@me.com>
2014-12-17 00:33:13 +0100

    Add base project

---
+++ Source/NSManagedObject+ANDYObjectIDs.h
@@ -0,0 +1,13 @@
+//
+//  NSManagedObject+ANDYObjectIDs.h
+//  Demo
+//
+//  Created by Elvis Nuñez on 12/17/14.
+//  Copyright (c) 2014 ANDY. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObject (ANDYObjectIDs)
+
+@end
---
+++ Source/NSManagedObject+ANDYObjectIDs.m
@@ -0,0 +1,13 @@
+//
+//  NSManagedObject+ANDYObjectIDs.m
+//  Demo
+//
+//  Created by Elvis Nuñez on 12/17/14.
+//  Copyright (c) 2014 ANDY. All rights reserved.
+//
+
+#import "NSManagedObject+ANDYObjectIDs.h"
+
+@implementation NSManagedObject (ANDYObjectIDs)
+
+@end
